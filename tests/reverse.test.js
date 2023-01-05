5bef138c91d2eda7dd8e36777141d103b9afc6f7
Rustem Ishmakov <dartrust@gmail.com>
2023-01-05 13:20:45 +0300

    modified test to  assert.equal

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,7 +1,7 @@
+import { strict as assert } from 'node:assert';
+
 import { reverse } from '../src/reverse.js';
 
-if(reverse('hello')!== 'olleh'){
-    throw new Error ('Тесты не прошли');
-};
+assert.equal(reverse('hello'), 'olleh');
 
 console.log('Все тесты пройдены!');
